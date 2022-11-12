662c0bf8c6826ab102b30a291d56f72279080a15
canaydin263 <canaydin263@gmail.com>
2022-11-12 22:30:48 +0300

    Guncelleme

--- src/main/java/day30collections/LinkedList01.java
+++ src/main/java/day30collections/LinkedList01.java
@@ -36,7 +36,41 @@ public class LinkedList01 {
         System.out.println(firstElement);//Chris
         System.out.println(names);//[Alexandre, Mark, Tom, Jeremy, Hans]
 
-        names.element();
+        /*
+            Retrieves, but does not remove, the head (first element) of this list.
+            Throws: NoSuchElementException - if this list is empty (peek'ten farki)
+         */
+        String f=names.element();
+        System.out.println(f);//Alexandre
+        System.out.println(names);//[Alexandre, Mark, Tom, Jeremy, Hans]
+
+        //Example 1: "A" ile baslayan tum isimleri "*****" e ceviriniz.
+        LinkedList<String> students=new LinkedList<>();
+        students.add("Chris");
+        students.add("Alexandre");
+        students.add("Mark");
+        students.add("Tom");
+        students.add("Jeremy");
+        students.add("Hans");
+
+        System.out.println(students);
+        for (String w :students){
+            if (w.startsWith("A")){
+                students.set(students.indexOf(w),"*****");
+            }
+        }
+        System.out.println(students);
+
+        //Example 2: List'deki 4 harften cok harf iceren isimleri siliniz
+
+        for (int i=0; i<students.size(); i++){
+            if (students.get(i).length()>4){
+                students.remove(students.get(i));
+                i--;
+            }
+        }
+        System.out.println(students);
+        //eleman sayisini azaltacagimiz icin i ye ihtiyacimiz var ,o yuzden for-each loop degil for loop kullaniyoruz.
 
 
     }
